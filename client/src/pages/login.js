6f393f0e3b0ae8cbae620e9879d292dac62ecf10
poojathakkar <poojapthakkar@gmail.com>
2020-08-23 21:55:58 -0400

    added required fields

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -39,8 +39,6 @@ const Login = () => {
       console.log("1", res.data);
      auth.setUser(res.data);
      const state = { msg: 'Welcome!' };
-    //  <Redirect to='/studenthomepage' />
-
 
       if(res.data.role === 'student') {
         history.replace('/studenthomepage', state)
@@ -69,11 +67,11 @@ const Login = () => {
         <form onSubmit={handleSubmit} className="FormFields">
           <div className="FormField">
             <label className="FormField__Label" htmlFor="email">E-Mail Address</label>
-            <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
           </div>
           <div className="FormField">
             <label className="FormField__Label" htmlFor="password">Password</label>
-            <input type="password" id="password" className="FormField__Input" placeholder="Enter your password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <input type="password" id="password" className="FormField__Input" placeholder="Enter your password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
           </div>
           <div className="FormField">
             <button className="FormField__Button mr-20">Login</button> <Link to="/register" className="FormField__Link">Create an account</Link>
