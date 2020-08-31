d473d916d732d63adf250cce2f9c74cf0e54423a
poojathakkar <poojapthakkar@gmail.com>
2020-08-31 11:37:19 -0400

    search query on tags

--- backend/routes/searchCourse.js
+++ backend/routes/searchCourse.js
@@ -4,12 +4,22 @@ module.exports = db => {
   router.get('/:searchTerm', (req, res) => {
     const { title } = req.body;
     const searchTerm = req.params.searchTerm;
+    console.log(searchTerm);
+
     const query = {
-      text: "SELECT id, title, description, price, thumbnail, content FROM courses WHERE lower(title) like $1",
-      values: [`%${searchTerm}%`]
+      text: `SELECT id, title, description, price, thumbnail, content, tags
+      FROM courses WHERE $1 = ANY (tags)`,
+      values: [searchTerm]
     };
+    // const query = {
+    //   text: "SELECT id, title, description, price, thumbnail, content, tags FROM courses WHERE lower(title) like $1",
+    //   values: [`%${searchTerm}%`]
+    // };
     db.query(query)
-      .then(result => res.json(result))
+      .then(result => {
+        console.log(result)
+        res.json(result)
+      })
       .catch(err => console.log(err));
   });
   return router;
