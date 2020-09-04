4779a972753b22e4f5b3fc8499a6ce173019a134
poojathakkar <poojapthakkar@gmail.com>
2020-09-04 11:49:45 -0400

    final css changes

--- client/src/components/Report.js
+++ client/src/components/Report.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 function BarGroup(props) {
   const barPadding = 2
-  const barColour = '#348AA7'
+  const barColour = '#66DAC7'
   const widthScale = d => d * 2
   const width = widthScale(props.d.value)
   const yMid = props.barHeight * 0.5
@@ -19,6 +19,10 @@ function BarGroup(props) {
 function Report() {
 
   const barHeight = 30;
+  const styles = {
+    display: "flex",
+    margin: "0 auto"
+  };
   const [data, setData] = useState([]);
 
     useEffect(() => {
@@ -38,8 +42,8 @@ function Report() {
         <BarGroup d={d} barHeight={barHeight} />
       </g>)                         
     
-    return <div className="main">
-      <svg width="800" height="400" >
+    return <div className="main" >
+      <svg width="800" height="400" style={styles}>
       <g className="container">
         <text className="title" x="100" y="30">Monthly Revenue Report</text>
         <g className="chart" transform="translate(100,60)">
